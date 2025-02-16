2ce8e3266b73fce1eedccc19cc93c40651155f15
Abhay Singh <singhabhay3145@gmail.com>
2025-02-16 23:12:23 +0530

    .

--- Auth.py
+++ Auth.py
@@ -1057,5 +1057,6 @@ def studentdata():
 
 
 # Run the Flask App
-if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned PORT
+    socketio.run(app, host="0.0.0.0", port=port,allow_unsafe_werkzeug=True)
